af0ebe4057be99ccfcd112c92deee90dbaca8cf4
Jeff Puckett <jeff@jeffpuckett.com>
2017-07-19 03:06:20 +0000

    check if defined first

--- config/constants.php
+++ config/constants.php
@@ -1,6 +1,14 @@
 <?php
 
-define('TWITTER_CONSUMER_KEY', env('TWITTER_CONSUMER_KEY'));
-define('TWITTER_CONSUMER_SECRET', env('TWITTER_CONSUMER_SECRET'));
-define('OAUTH_TOKEN', env('OAUTH_TOKEN'));
-define('OAUTH_SECRET', env('OAUTH_SECRET'));
+if (!defined('TWITTER_CONSUMER_KEY')) {
+    define('TWITTER_CONSUMER_KEY', env('TWITTER_CONSUMER_KEY'));
+}
+if (!defined('TWITTER_CONSUMER_SECRET')) {
+    define('TWITTER_CONSUMER_SECRET', env('TWITTER_CONSUMER_SECRET'));
+}
+if (!defined('OAUTH_TOKEN')) {
+    define('OAUTH_TOKEN', env('OAUTH_TOKEN'));
+}
+if (!defined('OAUTH_SECRET')) {
+    define('OAUTH_SECRET', env('OAUTH_SECRET'));
+}
