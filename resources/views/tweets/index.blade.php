1a7e5c615990882d68e9a5fcc8a3214d1ee9ce94
Jeff Puckett <jeff@jeffpuckett.com>
2017-07-19 10:17:23 +0000

    add links to tweet and data

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -5,10 +5,14 @@
         @foreach ($tweets as $tweet)
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    <a href="{{ route('tweets.show', $tweet) }}">{{ $tweet->salutation }}</a>
+                    <strong>{{ $tweet->salutation }}</strong>
                 </div>
                 <div class="panel-body">
                     {{ $tweet->tweet }}
+                </div>
+                <div class="panel-footer">
+                    <a href="https://twitter.com/i/web/status/{{ $tweet->tweet_id }}" class="btn btn-default" target="_blank" rel="noopener noreferrer">Tweet</a>
+                    <a href="{{ route('tweets.show', $tweet) }}" class="btn btn-default" target="_blank">Data</a>
                 </div>
             </div>
         @endforeach
