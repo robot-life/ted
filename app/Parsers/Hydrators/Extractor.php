283d0652fbb84c8300b6c1d469d53e138ff7d521
Jeff Puckett <jeff@jeffpuckett.com>
2017-07-27 06:44:01 +0000

    fix strange twitter bug with no id in stream

--- app/Parsers/Hydrators/Extractor.php
+++ app/Parsers/Hydrators/Extractor.php
@@ -21,9 +21,14 @@ class Extractor implements Hydrator
      */
     public function hydrate(Tweet $tweet) : bool
     {
+        if (!isset($tweet->json->id, $tweet->json->text, $tweet->json->created_at)) {
+            return false;
+        }
+
         $tweet->id = $tweet->json->id;
         $tweet->text = $tweet->json->extended_tweet->full_text ?? $tweet->json->text;
         $tweet->created_at = new Carbon($tweet->json->created_at);
+
         return true;
     }
 }
